f6827cfd89326284c9c0d11d1c784230b0a53936
universegalaxy1112 <54145818+universegalaxy1112@users.noreply.github.com>
2022-12-13 15:49:29 +0800

    add search feature

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,47 @@
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { CartContext } from "../CartContext";
+import { ToastContainer } from "react-toastify";
+
+function ProductCard(props) {
+  const navigate = useNavigate();
+  const { addProductToCart } = useContext(CartContext);
+  const [size, setSize] = useState("");
+
+  return (
+    <>
+      {/*  */}
+      <div className=' card'>
+        <img
+          className='card-img-top img-fluid img-thumbnail align-self-center'
+          src={`/images/${props.product.photo}`}
+          alt='Card image cap'
+          onClick={(evt) => {
+            navigate(`/product/${props.product.id}`);
+          }}
+        />
+        <div className='card-body'>
+          <h5 className='card-title d-flex'>{props.product.name}</h5>
+          <div className='d-flex price-panel mb-5'>
+            <h6 className='card-text'>
+              ${props.product.price}
+            </h6>
+          </div>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              addProductToCart(1, size, props.product);
+            }}
+          >
+            <div>
+              <input type='submit' className='btn btn-outline-dark' value='Add to cart' />
+            </div>
+          </form>
+        </div>
+      </div>
+      <ToastContainer />
+    </>
+  );
+}
+
+export default ProductCard;
---
+++ src/components/Search.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect, useContext } from "react";
+import ProductCard from "./ProductCard";
+import { ProductContext } from "../ProductContext";
+
+function Search() {
+  const [searchText, setSearchText] = useState("");
+  const { products: allProducts, error } = useContext(ProductContext);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts(searchText).then(null);
+  }, [searchText]);
+
+  const fetchProducts = async () => {
+    if (searchText !== "") {
+      const filteredProduct = allProducts.filter(({ name, category }) => {
+        return name.toLowerCase().match(searchText.toLowerCase());
+      });
+      setProducts(filteredProduct);
+    } else {
+      setProducts(allProducts);
+    }
+  };
+  if (error) return error;
+
+  return (
+    <>
+      <h1 className='marginSearch'>Search</h1>
+      <hr />
+      <div className='container align-items-center filter'>
+        <input
+          type='text'
+          value={searchText}
+          placeholder='Search Product'
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+      </div>
+      {products.length > 0 ? (
+        <div className='col-12 col-sm-12 col-md d-flex flex-wrap align-items-center'>
+          {products.map((product) => {
+            return <ProductCard product={product} key={product.id} />;
+          })}
+        </div>
+      ) : (
+        <div className='col-12 col-sm col-md d-flex justify-content-center'>
+          <h4>No Search Result...</h4>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Search;
